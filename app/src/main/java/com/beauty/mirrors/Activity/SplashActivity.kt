13a17d86e47782d27837fe468e92fa36fd0b5b35
jitendra.neepanlok@gmail.com <jitendra.neepanlok@gmail.com>
2021-06-17 11:58:13 +0530

    project setup and design splash screen

---
+++ app/src/main/java/com/beauty/mirrors/Activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.beauty.mirrors.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.beauty.mirrors.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
